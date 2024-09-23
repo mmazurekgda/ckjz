1b7b8dba30ecc6749c82b5c117764bb8de6bf927
Michał Mazurek <michal.mazurek@johnnybros.com>
2024-09-23 19:43:34 +0200

    Update the script

--- ckjz/script/__main__.py
+++ ckjz/script/__main__.py
@@ -1,14 +1,14 @@
 import requests
 import random
 import time
-import json
+import click
+import gpio as GPIO
+import logging
 from ckjz.constants import TOILET_TYPE
 
-TARGET_IP_ADDRESS = 'localhost'
-TARGET_PORT = 3000
 
-def update(name: TOILET_TYPE, status: bool):
-    url = f'http://{TARGET_IP_ADDRESS}:{TARGET_PORT}/toilets/{name.value}'
+def update(name: TOILET_TYPE, status: bool, ip: str, port: int):
+    url = f'http://{ip}:{port}/toilets/{name.value}'
     response = requests.post(
         url,
         params={'status': status},
@@ -17,9 +17,65 @@ def update(name: TOILET_TYPE, status: bool):
     return response.json()
 
 
-if __name__ == '__main__':
+def add_toilet_type_options(function):
+    for toilet_type in TOILET_TYPE:
+        function = click.option(
+            f"--{toilet_type.value}",
+            default=None,
+            type=int,
+            required=False,
+            help=f"GPIO pin number for {toilet_type.value}"
+        )(function)
+    return function
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option('--ip', default='localhost', help='IP address of the server')
+@click.option('--port', default=3000, help='Port of the server')
+@click.option('--interval', default=1, help='Interval between updates')
+def test(ip, port, interval, **kwargs):
+    logger = logging.getLogger(__name__)
+    logger.info(f"Starting test with interval {interval}")
     while True:
         for name in TOILET_TYPE:
-            time.sleep(1)
-            update(name, random.choice([True, False]))
+            time.sleep(interval)
+            logger.info(f"Updating `{name}`")
+            status = random.choice([True, False])
+            update(name, status, ip, port)
+            logger.info(f"Updated `{name}` with status `{status}`")
 
+
+@click.command()
+@click.option('--ip', default='localhost', help='IP address of the server')
+@click.option('--port', default=3000, help='Port of the server')
+@click.option('--interval', default=1, help='Interval between updates')
+@add_toilet_type_options
+def run(ip, port, interval, **kwargs):
+    logger = logging.getLogger(__name__)
+    logger.info(f"Starting run with interval {interval}")
+    while True:
+        if not any(kwargs.values()):
+            logger.error("No pins provided!")
+            exit(1)
+        for name, pin in kwargs.items():
+            if pin is None:
+                continue
+            logger.info(f"Reading pin `{pin}` for `{name}`")
+            status = GPIO.input(pin)
+            logger.info(f"Updating `{name}` with status `{status}`")
+            update(name, status, ip, port)
+            logger.info(f"Updated `{name}` with status `{status}`")
+        logger.info(f"Sleeping for {interval} seconds")
+        time.sleep(interval)
+
+
+cli.add_command(test)
+cli.add_command(run)
+
+if __name__ == '__main__':
+    cli()
