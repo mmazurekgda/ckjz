3982f1903c20b5941eb8c6df3186e63abed7b34c
Michał Mazurek <michal.mazurek@johnnybros.com>
2024-09-24 14:19:50 +0200

    Refactor GPIOPin class to implement singleton pattern by pin

--- ckjz/script/__main__.py
+++ ckjz/script/__main__.py
@@ -4,7 +4,7 @@ import time
 import click
 import logging
 from ckjz.constants import TOILET_TYPE
-from ckjz.gpio import GPIOPin, IN
+import RPi.GPIO as GPIO
 
 
 def update(name: str, status: bool, ip: str, port: int):
@@ -56,17 +56,23 @@ def test(ip, port, interval, **kwargs):
 @click.option("--interval", default=1, help="Interval between updates")
 @add_toilet_type_options
 def run(ip, port, interval, **kwargs):
+    GPIO.setmode(GPIO.BCM)
     logger = logging.getLogger(__name__)
     logger.info(f"Starting run with interval {interval}")
     if not any(kwargs.values()):
         logger.error("No pins provided!")
         exit(1)
+    for name, pin in kwargs.items():
+        if pin is None:
+            continue
+        logger.info(f"Setting up pin `{pin}` for `{name}`")
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     while True:
         for name, pin in kwargs.items():
             if pin is None:
                 continue
             logger.info(f"Reading pin `{pin}` for `{name}`")
-            status = GPIOPin(pin=pin, direction=IN).read()
+            status = GPIO.input(pin)
             logger.info(f"Updating `{name}` with status `{status}`")
             update(name, status, ip, port)
             logger.info(f"Updated `{name}` with status `{status}`")
